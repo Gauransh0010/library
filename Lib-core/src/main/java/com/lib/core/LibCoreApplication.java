9cc37622cb53a42e78f23f84bd96440c3bd7f877
Gauransh10 <101570842+Gauransh10@users.noreply.github.com>
2024-04-14 15:21:49 +0530

    initial project setup

---
+++ Lib-core/src/main/java/com/lib/core/LibCoreApplication.java
@@ -0,0 +1,13 @@
+package com.lib.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibCoreApplication.class, args);
+	}
+
+}
